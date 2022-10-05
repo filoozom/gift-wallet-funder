d7b8ec1ce95f9df338e8d43266c7abb63abe8c48
Philippe Schommers <philippe@schommers.be>
2022-10-05 09:28:59 +0200

    feat: make config via env variables

--- index.js
+++ index.js
@@ -1,41 +1,55 @@
-import { getDefaultProvider, Wallet, utils, Contract } from "ethers";
+import { getDefaultProvider, Wallet, Contract } from "ethers";
 import { writeFile } from "fs/promises";
 
 // ABI
 import abi from "./abi.json" assert { type: "json" };
 import erc20 from "./erc20.json" assert { type: "json" };
 
+// Env
+const {
+  RPC,
+  PRIVATE_KEY,
+  CONTRACT_ADDRESS,
+  BZZ_ADDRESS,
+  TOKEN_AMOUNT,
+  NATIVE_AMOUNT,
+  WALLET_COUNT,
+} = process.env;
+
 const wallets = [];
 
 // Generate wallets
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < WALLET_COUNT; i++) {
   const { address, privateKey } = Wallet.createRandom();
   console.log(`${address}: ${privateKey}`);
   wallets.push({ address, privateKey });
 }
 
 // Write them to file
-await writeFile(`wallets-${Date.now()}.json`, JSON.stringify(wallets));
+await writeFile(`wallets/wallets-${Date.now()}.json`, JSON.stringify(wallets));
 
 // Wallet setup
-const provider = getDefaultProvider(process.env.RPC);
-const wallet = new Wallet(process.env.PRIVATE_KEY, provider);
-const funder = new Contract(process.env.CONTRACT_ADDRESS, abi, wallet);
+const provider = getDefaultProvider(RPC);
+const wallet = new Wallet(PRIVATE_KEY, provider);
+const funder = new Contract(CONTRACT_ADDRESS, abi, wallet);
 let tx;
 
 // Approve BZZ
-const token = new Contract(process.env.BZZ_ADDRESS, erc20, wallet);
+const token = new Contract(BZZ_ADDRESS, erc20, wallet);
 tx = await token.approve(
-  process.env.CONTRACT_ADDRESS,
-  BigInt(wallets.length) * 5n * 10n ** 15n
+  CONTRACT_ADDRESS,
+  BigInt(TOKEN_AMOUNT) * BigInt(wallets.length)
 );
 await tx.wait();
 console.log("Approve transaction mined");
 
 // Fund them
 tx = await funder.fund(
+  BZZ_ADDRESS,
+  TOKEN_AMOUNT,
+  NATIVE_AMOUNT,
   wallets.map(({ address }) => address),
-  { value: utils.parseEther("0.1").toBigInt() * BigInt(wallets.length) }
+  { value: BigInt(NATIVE_AMOUNT) * BigInt(wallets.length) }
 );
 
 await tx.wait();
